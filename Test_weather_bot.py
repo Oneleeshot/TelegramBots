15a9d983a5c2438966d8fa8f1d24f40cc935b2cd
Oneleeshot <instal9595@gmail.com>
2021-02-21 18:48:11 +0300

    Add WetaherBot

---
+++ Test_weather_bot.py
@@ -0,0 +1,28 @@
+import telebot
+import tokens
+from pyowm import OWM
+
+bot = telebot.TeleBot(tokens.test3456_weather_bot_token)
+owm = OWM('1a2f72ddc7717e5a15fdb78e46523bda')
+mgr = owm.weather_manager()
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, введи название города.')
+
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    city = mgr.weather_at_place(message.text)
+    city_weather_temperature = city.weather.temperature(
+        'celsius')['temp']
+    city_weather_wind = city.weather.wind()
+    city_weather_humidity = city.weather.humidity
+    bot.send_message(message.chat.id,
+                     f'Температура: {city_weather_temperature} градусов\n'
+                     f"Скорость ветра: {city_weather_wind['speed']} метров в секунду\n"
+                     f"Влажность: {city_weather_humidity}%")
+
+
+bot.polling()
